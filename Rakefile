ec7f91bc7d4e746091d174a441266aca8ae5b4ca
Sinclair Target <sinclairtarget@gmail.com>
2019-03-10 13:22:02 -0500

    Revise data pipeline.

--- Rakefile
+++ Rakefile
@@ -1,18 +1,24 @@
 require 'rake/clean'
 
-JS_SOURCE_FILES = FileList["src/*.js"]
+STATIC_FOLDER = 'dist/static'
+JSON_DATA_FILE = 'dist/static/data.json'
 
-task :default => :build
+desc 'Generate JSON data file'
+task :convert => JSON_DATA_FILE
 
-desc 'Bundle javascript'
-task :build => 'dist/main.js'
+file JSON_DATA_FILE => ['sectors.json', 'industries.json', STATIC_FOLDER] do |t|
+  sh %{ jq -s '{sectors: .[0], industries: .[1]}' sectors.json industries.json > #{t.name}}
+end
+CLOBBER.include(JSON_DATA_FILE)
+
+rule(/(sectors|industries).json/) do
+  sh 'Rscript convert-data.r'
+end
+CLEAN.include('sectors.json', 'industries.json')
+
+directory STATIC_FOLDER
 
 desc 'Deploy to sinclairtarget.com'
-task :deploy => 'dist/main.js' do
+task :deploy do
   sh 'scp dist/* sinclair@sinclairtarget.com:/srv/www/sinclairtarget.com/concentration'
 end
-
-file 'dist/main.js' => JS_SOURCE_FILES do
-  sh 'yarn build'
-end
-CLOBBER.include('dist/main.js')
--- convert-data.r
+++ convert-data.r
@@ -63,46 +63,77 @@ read_all_concentration_data <- function(dir = ".", pattern = NULL) {
     df
 }
 
-# Shape each dataframe down to what we need
-shape <- function(df) {
-    df <- df %>%
-          filter(nchar(NAICS.id) == 4) %>%      # Only look at industry groups
-          filter(!is.na(VAL_PCT))
+industries <- function(df) {
+    df.industries <- df %>%
+        filter(nchar(NAICS.id) == 6) %>%      # Only look at industries
+        filter(!is.na(VAL_PCT)) %>%
+        filter(CONCENFI.id == '804')  %>%     # Only look at top four
+        select(NAICS.id, NAICS.label, SECTOR.id, SECTOR.label, VAL_PCT)
 
-    df <- df %>%
-          select(NAICS.id, NAICS.label, CONCENFI.id,
-                 SECTOR.label, RCPTOT, VAL_PCT) %>%
-          filter(CONCENFI.id == '804')  %>%     # Only look at top four
-          select(-CONCENFI.id)
+    # Join with rcptot for all firms
+    df.rcptot <- df %>%
+        filter(nchar(NAICS.id) == 6) %>%      # Only look at industries
+        filter(CONCENFI.id == '001')  %>%     # Only look at all firms
+        select(NAICS.id, RCPTOT) %>%
+        rename(RCPTOT_ALL_FIRMS = RCPTOT)
+
+    inner_join(df.industries, df.rcptot, by=c('NAICS.id'))
 }
 
-df.1997 <- read_csv('concentration-data/1997/industry-groups.csv') %>%
-           filter(!is.na(VAL_PCT)) %>%
-           mutate(NAICS.id = as.character(NAICS.id))
+sectors <- function(df) {
+    df.sectors <- df %>%
+        filter(nchar(NAICS.id) == 2) %>%      # Only look at sectors
+        filter(CONCENFI.id == '001')  %>%     # Only look at all firms
+        select(SECTOR.id, SECTOR.label, RCPTOT) %>%
+        rename(RCPTOT_ALL_FIRMS = RCPTOT)
 
-df.2002 <- read_all_concentration_data('concentration-data/2002', '*Z6.csv')
-df.2007 <- read_all_concentration_data('concentration-data/2007', '*Z6.csv')
-df.2012 <- read_all_concentration_data('concentration-data/2012', '*Z6.csv')
+    # Calculate mean VAL_PCT of sector industries
+    df.means <- df %>%
+                industries() %>%
+                group_by(SECTOR.id, SECTOR.label) %>%
+                summarize(MEAN_VAL_PCT = mean(VAL_PCT))
 
-df.2002 <- shape(df.2002)
-df.2007 <- shape(df.2007)
-df.2012 <- shape(df.2012)
+    inner_join(df.sectors, df.means, by=c('SECTOR.id', 'SECTOR.label'))
+}
+
+#df.1997 <- read_csv('concentration-data/1997/industry-groups.csv') %>%
+#           filter(!is.na(VAL_PCT)) %>%
+#           mutate(NAICS.id = as.character(NAICS.id))
+
+df.raw.2002 <- read_all_concentration_data('concentration-data/2002', '*Z6.csv')
+df.raw.2007 <- read_all_concentration_data('concentration-data/2007', '*Z6.csv')
+df.raw.2012 <- read_all_concentration_data('concentration-data/2012', '*Z6.csv')
+
+df.2002 <- industries(df.raw.2002)
+df.2007 <- industries(df.raw.2007)
+df.2012 <- industries(df.raw.2012)
+
+df.sectors.2002 <- sectors(df.raw.2002)
+df.sectors.2007 <- sectors(df.raw.2007)
+df.sectors.2012 <- sectors(df.raw.2012)
 
 df <- inner_join(df.2002,
                  df.2007,
-                 by=c('NAICS.id', 'NAICS.label', 'SECTOR.label'),
+                 by=c('NAICS.id', 'NAICS.label', 'SECTOR.id', 'SECTOR.label'),
                  suffix=c('.2002', '.2007'))
 df <- inner_join(df,
                  df.2012,
-                 by=c('NAICS.id', 'NAICS.label', 'SECTOR.label')) %>%
+                 by=c('NAICS.id', 'NAICS.label', 'SECTOR.id', 'SECTOR.label')) %>%
       rename(VAL_PCT.2012 = VAL_PCT,
-             RCPTOT.2012 = RCPTOT)
+             RCPTOT_ALL_FIRMS.2012 = RCPTOT_ALL_FIRMS)
 
-df <- inner_join(df,
-                 df.1997,
-                 by=c('NAICS.id', 'NAICS.label')) %>%
-      rename(VAL_PCT.1997 = VAL_PCT,
-             RCPTOT.1997 = RCPTOT)
+df.sectors <- inner_join(df.sectors.2002,
+                         df.sectors.2007,
+                         by=c('SECTOR.id', 'SECTOR.label'),
+                         suffix=c('.2002', '.2007'))
+df.sectors <- inner_join(df.sectors,
+                         df.sectors.2012,
+                         by=c('SECTOR.id', 'SECTOR.label')) %>%
+              rename(MEAN_VAL_PCT.2012 = MEAN_VAL_PCT,
+                     RCPTOT_ALL_FIRMS.2012 = RCPTOT_ALL_FIRMS)
 
 json <- toJSON(df, pretty=TRUE)
-write(json, 'dist/data.json')
+write(json, 'industries.json')
+
+json <- toJSON(df.sectors, pretty=TRUE)
+write(json, 'sectors.json')
