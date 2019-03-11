20568351cafe8184e3ebddd5ed06a948a7347ffd
Sinclair Target <sinclairtarget@gmail.com>
2019-03-11 16:24:59 -0500

    Remove placeholder tooltips.

--- src/js/storynode.js
+++ src/js/storynode.js
@@ -35,9 +35,7 @@ export class DrawSectorsNode extends StoryNode {
            .attr("class", d => "sector-" + d["SECTOR.id"])
            .attr("r", 0)
            .attr("cx", d => scatter.xScale(k(d, "MEAN_VAL_PCT", 2002)))
-           .attr("cy", d => scatter.yScale(k(d, "MEAN_VAL_PCT", this.toYear)))
-           .append("title")
-           .text(d => d["SECTOR.label"]);
+           .attr("cy", d => scatter.yScale(k(d, "MEAN_VAL_PCT", this.toYear)));
 
     this.transition(data,
                     scatter,
