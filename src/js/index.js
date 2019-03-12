6264202a18a27b4b5a036858186daad4845c4f15
Sinclair Target <sinclairtarget@gmail.com>
2019-03-11 23:23:00 -0500

    require() -> import for waypoints lib.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import * as d3 from "d3";
-require('waypoints');
+import "waypoints";
 import Dimensions from "./dimensions.js";
 import Scatter from "./scatter.js";
 import Dataset from "./dataset.js";
